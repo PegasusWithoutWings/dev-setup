c8ee6052b4d8b037f5bcbc7e4042929ea99242c7
TaoOfCoding <georgezhangtj97@gmail.com>
2017-08-25 03:08:04 -0700

    conda.sh will now install Anaconda2 and Anaconda3, while creating environments for Python2 and Python3.

--- conda.sh
+++ conda.sh
@@ -8,12 +8,21 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Check for Anaconda,
+# Check for Miniconda,
 # Install if we don't have it
 if test ! $(which conda); then
-  echo "Installing Anaconda..."
+  echo "Installing Miniconda..."
   wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
-  bash Miniconda3-latest-MacOSX-x86_64.sh
-  # TODO: Export Conda's location to PATH variable.
+  bash Miniconda3-latest-MacOSX-x86_64.sh -bf
+  echo 'export PATH="$HOME/miniconda3/bin:$PATH"' >> $HOME/.zshenv
   rm -f Miniconda3-latest-MacOSX-x86_64.sh
 fi
+
+# Update conda to make sure we have the most up-to-date version
+conda update -y conda
+
+# Install Anaconda3 and create py3k environment with conda
+conda create -ny py3k anaconda python=3
+
+# Install Anaconda2 and create py2k environment with conda
+conda create -ny py2k anaconda python=2
