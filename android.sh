6743843f25f88d87192a7ccd88b231662e47f7c5
Pegasus Without Wings <georgezhangtj97@gmail.com>
2017-12-13 02:56:24 -0800

    Updated web.sh to now reflect only used features.

--- android.sh
+++ android.sh
@@ -23,10 +23,10 @@ brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
 
 brew cask install --appdir="~/Applications" java
-brew cask install --appdir="~/Applications" Caskroom/versions/intellij-idea-ce
-brew cask install --appdir="~/Applications" android-studio
+# brew cask install --appdir="~/Applications" Caskroom/versions/intellij-idea-ce
+# brew cask install --appdir="~/Applications" android-studio
 
-brew install android-sdk
+# brew install android-sdk
 
 # Remove outdated versions from the cellar.
 brew cleanup
