5652c3d338f6d7b18026c94e1b72a89520b00aaf
Pegasus Without Wings <georgezhangtj97@gmail.com>
2017-12-13 02:55:50 -0800

    Updated web.sh to now reflect only used features.

--- web.sh
+++ web.sh
@@ -18,14 +18,14 @@ fi
 # Make sure we’re using the latest Homebrew.
 brew update
 
-brew install node
+# brew install node
 
 # Remove outdated versions from the cellar.
 brew cleanup
 
-npm install -g coffee-script
-npm install -g grunt-cli
-npm install -g jshint
-npm install -g less
+# npm install -g coffee-script
+# npm install -g grunt-cli
+# npm install -g jshint
+# npm install -g less
 
 #gem install jekyll
