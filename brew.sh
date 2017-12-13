f83f08a9a1e9ffa3204f4e6210bc9fdec693a906
Pegasus Without Wings <georgezhangtj97@gmail.com>
2017-12-13 03:10:31 -0800

    brew.sh now installs flux.

--- brew.sh
+++ brew.sh
@@ -163,6 +163,7 @@ brew cask install --appdir="/Applications" 1password
 #brew cask install --appdir="/Applications" gimp
 #brew cask install --appdir="/Applications" inkscape
 brew cask install --appdir="/Applications" spectacle
+brew cask install --appdir="/Applications" flux
 
 #Remove comment to install LaTeX distribution MacTeX
 brew cask install --appdir="/Applications" mactex
